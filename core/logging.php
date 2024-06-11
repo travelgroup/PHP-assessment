efd56bec4e2e2fb40c3f2299087a97b1d3e71b4d
prkmuza <russmuza@gmail.com>
2024-06-11 09:58:04 +0200

    bug fixes

--- core/logging.php
+++ core/logging.php
@@ -4,4 +4,6 @@ namespace interview;
 
 class Logging {
 public static function logDBErrorAndExit($error) { die('An Error Occurred: ' . $error); }
+
+}
     //--------------------------------------------------------------------------
